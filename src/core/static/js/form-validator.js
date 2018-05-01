4cb1ee99940768c01faadea8cd197e296ef71457
Matthew Bridges <mattbridgesbusiness@gmail.com>
2018-05-01 20:49:01 +0100

    add submit handler that aggregates form data, and posts to current url

--- src/core/static/js/form-validator.js
+++ src/core/static/js/form-validator.js
@@ -1,6 +1,35 @@
 class FormView {
-    constructor() {
-        console.log('working');
+    constructor(container) {
+        this.container = container;
+        this.submit = container.querySelector('[type="submit"]');
+
+        this.handleFormSubmission = this.handleFormSubmission.bind(this);
+        this.registerEventListeners();
+    }
+
+    registerEventListeners() {
+        this.submit.addEventListener('click', this.handleFormSubmission.bind(this))
+    };
+
+    handleFormSubmission(e) {
+        e.preventDefault();
+        let data = {};
+
+        [...this.container.elements].forEach((field) => {
+            data[field.name] = field.value;
+        });
+    }
+
+    validateForm(data) {
+        fetch(window.location.pathname, {
+            method: 'POST',
+            headers: {
+                'content-type': 'application/x-www-form-urlencoded'
+            }
+        })
+        .then((response) => {
+            console.log(response);
+        })
     }
 }
 
