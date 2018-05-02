6d3bd010c35ec65b35f19cedf53273c9a3f6201a
Matthew Bridges <mattbridgesbusiness@gmail.com>
2018-05-02 17:06:26 +0100

    figure out headers for fetch request

--- src/core/static/js/form-validator.js
+++ src/core/static/js/form-validator.js
@@ -18,18 +18,51 @@ class FormView {
         [...this.container.elements].forEach((field) => {
             data[field.name] = field.value;
         });
+
+        this.validateForm(data);
     }
 
     validateForm(data) {
+        console.log(data);
         fetch(window.location.pathname, {
             method: 'POST',
+            credentials: 'same-origin',
             headers: {
-                'content-type': 'application/x-www-form-urlencoded'
-            }
+                'Content-Type': 'application/x-www-form-urlencoded',
+                'X-CSRFToken': this.getCSRFToken(),
+                'X-Requested-With': 'XMLHttpRequest'
+            },
+            body: data
         })
         .then((response) => {
-            console.log(response);
+            return Promise.resolve(response.json())
         })
+        .then(function(data) {
+            console.log('data', data);
+        })
+        .catch(function(err) {
+            console.log('err', err);
+        })
+    }
+
+
+    /***************\
+     * Utility Code *
+    \****************/
+
+    getCSRFToken() {
+        let cookieValue = null;
+        if (document.cookie && document.cookie !== '') {
+            let cookies = document.cookie.split(';');
+            for (let i = 0; i < cookies.length; i++) {
+                let cookie = cookies[i].trim();
+                if (cookie.substring(0, 10) === ('csrftoken' + '=')) {
+                    cookieValue = decodeURIComponent(cookie.substring(10));
+                    break;
+                }
+            }
+        }
+        return cookieValue;
     }
 }
 
